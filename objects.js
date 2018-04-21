f5b693d6253ee539c28fd3d695a839b0e48bf66c
malorie16 <malorie18@gmail.com>
2018-04-21 17:43:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,8 @@
 var playlist = {beyonce: 'diva'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, {[artistName]: songTitle})
+
+}
+['Slowdive'] = 'Alison'
+playlist['My Bloody Valentine'] = 'Sometimes'
